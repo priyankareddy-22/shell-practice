1b1a8ba8fa5be771ed6c011941d9d2e3e2716082
priyanka <info@joindevops.com>
2025-05-24 13:10:08 +0530

    hello world started

---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Enter your pin Number::"
+read PIN 
+
+echo "Yournumber is: $PIN"
---
+++ 06-data-type.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Number1=100
+Number2=200
+
+TIMESTAMP=$(date)
+echo "Script executed at: $TIMSTAMP"
+#SUM=Number1+Number2
