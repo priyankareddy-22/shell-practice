0c0610100cda6bd2fcbd856d402d53243d1c8796
priyanka <info@joindevops.com>
2025-05-25 15:31:45 +0530

    special vars

---
+++ 10-install-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if[ $USERID -ne 0 ]
+then
+   echo "ERROR:: Please run the script with root access"
+   else
+    echo "You are running with root access"
+    fi
+
+    dnf insatll mysql -y
